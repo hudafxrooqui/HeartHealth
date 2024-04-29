999c1e8ad21a72b61b46cf7d96a9cd2fbe71995f
hudafxrooqui <96451714+hudafxrooqui@users.noreply.github.com>
2024-04-29 06:01:27 +0100

    Add files via upload

---
+++ accounts/admin.py
@@ -0,0 +1,8 @@
+# Import the admin module from Django.
+from django.contrib import admin
+from accounts.models import UserProfileInfo
+
+class UserProfile(admin.ModelAdmin):
+    list_display = ('user', 'profile_pic')
+admin.site.register(UserProfileInfo,UserProfile)
+# Register your models here.
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, re_path
+
+from . import views
+app_name = 'accounts'
+urlpatterns=[
+    re_path(r'^register/$', views.register, name='register'),
+    re_path(r'^logout/$',views.user_logout,name='logout'),
+    re_path(r'^profile/(?P<pk>\d+)/$', views.ProfileDetailView.as_view(), name='profile'),
+#url(r'^profile/(?P<pk>\d+)/edit/$', views.profile_update, name='edit_profile'),
+]
